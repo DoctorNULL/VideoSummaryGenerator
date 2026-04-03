0307c550317cb69542f213634c3e3f882b9eda99
MiDO Mostafa <mohamed.01061@gmail.com>
2026-04-03 06:57:57 +0200

    Modifying Controller Accessibility

--- controller/__init__.py
+++
@@ -1,3 +0,0 @@
-from .ui_controller import pick_video
-from .video_controller import extract_audio
-from .ai_controller import extract_transcript
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tkinter as tk
-from controller import pick_video, extract_audio, extract_transcript
+from controllers import ai_controller, ui_controller, video_controller
 
 def convert():
-    extract_audio(text.get("1.0", "end"))
-    extract_transcript()
+    video_controller.extract_audio(text.get("1.0", "end"))
+    ai_controller.extract_transcript()
 
 app = tk.Tk()
 app.title("Video Summarizer App")
@@ -12,7 +12,7 @@ app.geometry("500x300")
 text = tk.Text(app, height=5, width=40)
 text.pack(pady=10)
 
-picker = tk.Button(app, text="Pick", command=lambda : pick_video(text))
+picker = tk.Button(app, text="Pick", command=lambda : ui_controller.pick_video(text))
 picker.pack(pady=5)
 
 btn = tk.Button(app, text="Convert", command= convert)
